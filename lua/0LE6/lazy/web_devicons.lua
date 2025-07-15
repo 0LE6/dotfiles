155980aeb5dbb02c33f2b37dbe65fb8a46c9da64
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-15 22:00:44 +0200

    --help

--- lua/0LE6/lazy/web_devicons.lua
+++ lua/0LE6/lazy/web_devicons.lua
@@ -11,8 +11,44 @@ return {
           color = "#89e051",
           cterm_color = "113",
           name = "Shell"
-        }
-      },
+        },
+        rs = {
+          icon = "",
+          color = "#dea584",
+          cterm_color = "173",
+          name = "Rust"
+        }, 
+        html = {
+          icon = "",              
+          color = "#e34c26",
+          cterm_color = "202",
+          name = "Html"
+        },
+        css = {
+          icon = "",              
+          color = "#264de4",
+          cterm_color = "68",
+          name = "Css"
+        },
+        js = {
+          icon = "",              
+          color = "#f7df1e",
+          cterm_color = "226",
+          name = "JavaScript"
+        },
+        ts = {
+          icon = "",              
+          color = "#3178c6",
+          cterm_color = "68",
+          name = "TypeScript"
+        },
+        json = {
+          icon = "",              
+          color = "#cbcb41",
+          cterm_color = "185",
+          name = "Json"
+        },
+      },      
       default = true
     })
   end}
