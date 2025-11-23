bae389710c4963f232ffefb5ef94e9633d951fcc
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-23 20:35:46 +0100

    feat: add settings lua file

---
+++ lua/config/settings.lua
@@ -0,0 +1,30 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+
+vim.opt.clipboard = "unnamedplus"
+
+-- code folding using tree-sitter
+-- zc: fold lvl | zo: open lvl | zM: close all | zR: open all
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+
+-- hide the default vim status bar under the lualine 
+vim.opt.cmdheight = 0
+
+-- vertical line cursor
+vim.opt.guicursor = {
+  "n-v-c-sm-i-ci-ve-r-cr-o:ver25-blinkon1"
+}
+
+-- No automatic comment insertion
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
