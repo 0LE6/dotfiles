f8e2201ddbb4666d40f385bc6918f1d01d8eb00c
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-23 20:37:02 +0100

    feat: add init lua files

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("config")
+
+-- Suppress "Press ENTER or type command 
+-- to continue" messages
+vim.o.shortmess = vim.o.shortmess .. "I"
---
+++ lua/config/init.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+
+require("config.remap")
+require("config.settings")
+require("config.lazy_init")
+require("config.term")
