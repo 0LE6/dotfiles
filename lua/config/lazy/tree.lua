82ac330343b52ebe722a0d35a3ac272f8c57849e
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-04-20 21:26:10 +0200

    fix: update tree sitter adapting to 0.12 version

--- lua/config/lazy/tree.lua
+++ lua/config/lazy/tree.lua
@@ -1,37 +1,37 @@
+# Adapted to NVIM 0.12
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    opts = {
-        ensure_installed = {
-            "angular",
-            "bash",
-            "c",
-            "cmake",
-            "cpp",
-            "css",
-            "dockerfile",
-            "go",
-            "html",
-            "java",
-            "javascript",
-            "json",
-            "lua",
-            "markdown", 
-            "markdown_inline",
-            "matlab",
-            "python",
-            "rust",
-            "vim"
-            
-        },
-        indent = {
-            enable = true
-        },
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-    },
-}
+    config = function()
+    local ts = require("nvim-treesitter")
 
+    ts.setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    ts.install({
+        "bash",
+        "c", "cmake", "cpp", "css",
+        "dockerfile",
+        "go",
+        "html",
+        "java", "javascript", "json",
+        "lua",
+        "python",
+        "rust",
+        "vim",
+        "markdown", "markdown_inline",
+        "vimdoc",
+        "query",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+            pcall(vim.treesitter.start, args.buf)
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+    })
+    end,
+}
